e479cddc023c535b3bf3a7c9ee5c2a5c54596a6e
andrg2280 <andrg2280.mail.ru>
2023-08-29 16:53:24 +0300

    bug fixd2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -19,69 +19,80 @@ public class App {
         System.out.println("Hello, " + getUserName() + "!");
     };
     public static void firstInput() {
-        boolean bExit = false;
-        while (!bExit) {
-            System.out.println("Please enter the game number and press Enter.");
-            System.out.println("1 - Greet");
-            System.out.println("2 - Even");
-            System.out.println("3 - Calc");
-            System.out.println("4 - GDC");
-            System.out.println("5 - Progression");
-            System.out.println("6 - Prime");
-            System.out.println("0 - Exit");
-            Scanner scanner = new Scanner(System.in);
-            String selectedGame = scanner.next();
-            int iInput = Integer.parseInt(selectedGame);
-            switch (iInput) {
-                case 0 -> System.exit(0);
-                case 1 -> {
-                    greet();
-                    System.exit(0);
-                }
-                case 2 -> {
-                    greet();
-                    System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GDC");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        String selectedGame = scanner.next();
+        int iInput = Integer.parseInt(selectedGame);
+        switch (iInput) {
+            case 0 -> System.exit(0);
+            case 1 -> {
+                greet();
+                System.exit(0);
+            }
+            case 2 -> {
+                greet();
+                System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+                for (int i = 0; i < 3; i++) {
                     if (Even.even()) {
                         System.out.println("Congratulations, " + sUserName + "!");
                     } else {
-                        bExit = true;
+                        System.exit(0);
                     }
                 }
-                case 3 -> {
-                    greet();
-                    System.out.println("What is the result of the expression?");
+            }
+            case 3 -> {
+                greet();
+                System.out.println("What is the result of the expression?");
+                for (int i = 0; i < 3; i++) {
                     if (Calc.calc()) {
                         System.out.println("Congratulations, " + sUserName + "!");
+                    } else {
+                        System.exit(0);
                     }
                 }
-                case 4 -> {
-                    greet();
+            }
+            case 4 -> {
+                greet();
+                for (int i = 0; i < 3; i++) {
                     if (Nod.nod()) {
                         System.out.println("Congratulations, " + sUserName + "!");
+                    } else {
+                        System.exit(0);
                     }
                 }
-                case 5 -> {
-                    greet();
-                    System.out.println("What number is missing in the progression?");
+            }
+            case 5 -> {
+                greet();
+                System.out.println("What number is missing in the progression?");
+                for (int i = 0; i < 3; i++) {
                     if (Prog.prog()) {
                         System.out.println("Congratulations, " + sUserName + "!");
                     } else {
-                        bExit = true;
+                        System.exit(0);
                     }
                 }
-                case 6 -> {
-                    greet();
-                    System.out.println("Your choice: 6");
-                    System.out.println();
-                    System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+            }
+            case 6 -> {
+                greet();
+                System.out.println("Your choice: 6");
+                System.out.println();
+                System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+                for (int i = 0; i < 3; i++) {
                     if (Prime.prime()) {
                         System.out.println("Congratulations, " + sUserName + "!");
                     } else {
-                        bExit = true;
+                        System.exit(0);
                     }
                 }
-                default -> {
-                }
+            }
+            default -> {
             }
         }
     }
