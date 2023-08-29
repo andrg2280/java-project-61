86c0640740c135d237866c51d09bc3e51ebdc93b
andrg2280 <andrg2280.mail.ru>
2023-08-29 16:18:01 +0300

    bug fixed

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class App {
     static String sUserName = "";
     public static String getUserName() {
-        Scanner scanner = new Scanner(System.in);
-        sUserName = scanner.next();
+        if (sUserName.isEmpty()) {
+            Scanner scanner = new Scanner(System.in);
+            sUserName = scanner.next();
+        }
         return sUserName;
     }
     public static void main(String[] args) {
@@ -17,58 +19,67 @@ public class App {
         System.out.println("Hello, " + getUserName() + "!");
     };
     public static void firstInput() {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GDC");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
-        Scanner scanner = new Scanner(System.in);
-        String selectedGame = scanner.next();
-        int iInput = Integer.parseInt(selectedGame);
-        switch (iInput) {
-            case 0 -> System.exit(0);
-            case 1 -> greet();
-            case 2 -> {
-                greet();
-                System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-                if (Even.even()) {
-                    System.out.println("Congratulations, " + sUserName + "!");
+        boolean bExit = false;
+        while (!bExit) {
+            System.out.println("Please enter the game number and press Enter.");
+            System.out.println("1 - Greet");
+            System.out.println("2 - Even");
+            System.out.println("3 - Calc");
+            System.out.println("4 - GDC");
+            System.out.println("5 - Progression");
+            System.out.println("6 - Prime");
+            System.out.println("0 - Exit");
+            Scanner scanner = new Scanner(System.in);
+            String selectedGame = scanner.next();
+            int iInput = Integer.parseInt(selectedGame);
+            switch (iInput) {
+                case 0 -> System.exit(0);
+                case 1 -> greet();
+                case 2 -> {
+                    greet();
+                    System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+                    if (Even.even()) {
+                        System.out.println("Congratulations, " + sUserName + "!");
+                    } else {
+                        bExit = true;
+                    }
                 }
-            }
-            case 3 -> {
-                greet();
-                System.out.println("What is the result of the expression?");
-                if (Calc.calc()) {
-                    System.out.println("Congratulations, " + sUserName + "!");
+                case 3 -> {
+                    greet();
+                    System.out.println("What is the result of the expression?");
+                    if (Calc.calc()) {
+                        System.out.println("Congratulations, " + sUserName + "!");
+                    }
                 }
-            }
-            case 4 -> {
-                greet();
-                if (Nod.nod()) {
-                    System.out.println("Congratulations, " + sUserName + "!");
+                case 4 -> {
+                    greet();
+                    if (Nod.nod()) {
+                        System.out.println("Congratulations, " + sUserName + "!");
+                    }
                 }
-            }
-            case 5 -> {
-                greet();
-                System.out.println("What number is missing in the progression?");
-                if (Prog.prog()) {
-                    System.out.println("Congratulations, " + sUserName + "!");
+                case 5 -> {
+                    greet();
+                    System.out.println("What number is missing in the progression?");
+                    if (Prog.prog()) {
+                        System.out.println("Congratulations, " + sUserName + "!");
+                    } else {
+                        bExit = true;
+                    }
                 }
-            }
-            case 6 -> {
-                greet();
-                System.out.println("Your choice: 6");
-                System.out.println();
-                System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-                if (Prime.prime()) {
-                    System.out.println("Congratulations, " + sUserName + "!");
+                case 6 -> {
+                    greet();
+                    System.out.println("Your choice: 6");
+                    System.out.println();
+                    System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+                    if (Prime.prime()) {
+                        System.out.println("Congratulations, " + sUserName + "!");
+                    } else {
+                        bExit = true;
+                    }
                 }
-            }
-            default -> {
+                default -> {
+                }
             }
         }
-    };
+    }
 }
